36a86ceae74fb37984786f8269b492baa0bfd9b5
Cullan Shewfelt <cullanshewfelt@github.com>
2017-04-22 16:24:14 -0700

    almost final, needs some layout tweaking, logic 100% complete

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,25 +1,72 @@
 $(document).ready(function() {
 
+    var object;
+    var dataBackup;
+    var switch1;
+    var switch2;
     var topics = ["Thom Yorke Dancing", "Jimmy Page", "Jimi Hendrix", "Clayton Kershaw", "Kicked In The Nuts", "Throwing Computer"]
 
-    for (i = 0; i < topics.length; i++) {
-        $("#buttons-div").append(`<button type='button' class='btn btn-primary btn-large'>${topics[i]}</button>`);
+    function printButtons() {
+        $("#buttons-div").html("");
+        for (i = 0; i < topics.length; i++) {
+            $("#buttons-div").append(`<button type='button' class='btn btn-primary btn-large'>${topics[i]}</button>`);
+        }
     }
+    printButtons();
 
-    $("button").on("click", function() {
+    $(document).on("click", ".btn", function() {
+        $("#gif-gallery").empty();
         var selectedGif = this.innerHTML;
-        console.log(this);
-        console.log(selectedGif);
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + selectedGif + "&api_key=dc6zaTOxFJmzC";
-        $.get(queryURL).done(function(response) {
 
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + selectedGif + "&limit=10&api_key=dc6zaTOxFJmzC";
+
+        $.get(queryURL).done(function(response) {
+            object = response;
             console.log(queryURL);
             for (i = 0; i < response.data.length; i++) {
-                $("#gif-gallery").append(`<img src="${response.data[i].images.fixed_height.url}">
-                	<br
-                	><p class="text center">Rating: ${response.data[i].rating}</p>`);
+
+                dataBackup = `${object.data[i].images.original.url}`
+
+                $("#gif-gallery").append(`<img class="gif" src="${object.data[i].images.original_still.url}" id="img" data-backup="${object.data[i].images.original.url}">
+                    <br>
+                    <p class="text center">Rating: ${object.data[i].rating}</p>`);
             }
         })
-    });
+    })
 
+    $(document).on("click", "#img", function() {
+
+        // we create variables that change the attributes of the selcted images
+        switch1 = $(this).attr("src")
+        switch2 = $(this).attr("data-backup")
+
+        $(this).attr("src", switch2);
+        $(this).attr("data-backup", switch1);
+    })
+
+    $(document).on("mouseenter", "#img", function() {
+
+        switch1 = $(this).attr("src")
+        switch2 = $(this).attr("data-backup")
+
+        $(this).attr("src", switch2);
+        $(this).attr("data-backup", switch1)
+    })
+
+    $(document).on("mouseleave", "#img", function() {
+
+        switch1 = $(this).attr("src")
+        switch2 = $(this).attr("data-backup")
+
+        $(this).attr("src", switch2);
+        $(this).attr("data-backup", switch1)   
+    })
+
+    $(document).on("click", "#add-gif", function(event) {
+
+        event.preventDefault();
+        var gif = $("#gif-input").val().trim();
+        topics.push(gif);
+        printButtons()
+    });
 })
